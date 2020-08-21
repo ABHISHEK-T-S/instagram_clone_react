2437ce37c972cb17dd601a7a80817dfa99533976
Abhishek T S <tsabhishek1999@gmail.com>
2020-08-21 08:02:00 +0530

    firebase auth added

--- src/Modal.js
+++ src/Modal.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, TextField } from '@material-ui/core';
 import './Modal.css'
-// import { auth } from './Firebase';
-
-
+import { auth } from './Firebase';
 
 
 
@@ -23,11 +21,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SimpleModal() {
-// the hooks for storeing values
-const [email  ,setEmail] = useState("");
-const [username ,setUserName] = useState("");
-const [password, setPassword] = useState("");
-
+  // the hooks for storeing values
+  const [email, setEmail] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [user ,setuser] = useState("");
 
 
   const classes = useStyles();
@@ -41,40 +39,75 @@ const [password, setPassword] = useState("");
   const handleClose = () => {
     setOpen(false);
   };
+
+  //data change on event
+
+  const updateUserName = (e) => {
+    setUserName(e.target.value)
+  };
+  const updateEmail = (e) => {
+    setEmail(e.target.value)
+  };
+  const updatePassword = (e) => {
+    setPassword(e.target.value)
+  };
+
+
+  //sign in and signup handlers
+
   const handleclickSignIn = () => {
     console.log('sign-in')
-    console.log(email , password , username)
+    console.log(email, password, username)
   };
 
-  const handleclickSignUp =()=>{
-    console.log("sign-up")
-  };
+  const handleclickSignUp = () => {
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authuser=>{
+      return authuser.user.updateProfile({
+        displayName:username
+      });
+    }))
+    .catch(function(error) {
+      // Handle Errors here.
+      console.log(error.code);
+     alert(error.message) ;
+      // ...
+    });  };
 
-const updateUserName = (e)=>{
-  setUserName(e.target.value)
-};
-const updateEmail =(e)=>{
-  setEmail(e.target.value)
-};
-const updatePassword = (e) =>{
-  setPassword(e.target.value)
-};
+//use Effect
 
 
 
-const body = (
-  
+  useEffect(() => {
+    auth.onAuthStateChanged((authuser) => {
+      if (authuser){
+        console.log(authuser);
+        setuser(authuser)
+      }
+      else{
+        setuser(null);
+      }
+    })
+  }, [])
+
+
+
+  const body = (
+
     <div className={classes.paper}>
       <img className="logo-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2JK42TiBH9apltQsgx9B5-qDGkeB9JvZ1dg&usqp=CAU" alt="logo" />
       <form className='sign-up'>
 
         <TextField id="outlined-basic1" label="UserName" variant="outlined" onChange={updateUserName} /><br></br><br />
         <TextField id="outlined-basic3" label="Email" variant="outlined" onChange={updateEmail} /><br></br><br />
-        <TextField id="outlined-basic2" label="Password" variant="outlined" type="password" onChange ={updatePassword} /><br></br>
+        <TextField id="outlined-basic2" label="Password" variant="outlined" type="password" onChange={updatePassword} /><br></br>
         <br /><br />
         <div className="signButtons">
-          <Button variant="contained"  onClick={handleclickSignUp} >sign up</Button>
-          <Button variant="contained" color="primary" onClick={handleclickSignIn} >sign in</Button>
+          {user ? (          <Button variant="contained" color="primary" onClick={handleclickSignUp} >sign out</Button>):
+          (<Button variant="contained" color="primary" onClick={handleclickSignUp} >sign up</Button> )
+          }
+          {/* <Button variant="contained" color="primary" onClick={handleclickSignUp} >sign up</Button> */}
+          {/* <Button variant="contained" color="primary" onClick={handleclickSignIn} >sign in</Button> */}
         </div>
       </form>
 
@@ -82,7 +115,7 @@ const body = (
   );
 
   return (
-    
+
     <div>
       <button type="button" onClick={handleOpen}>
         Sign up
